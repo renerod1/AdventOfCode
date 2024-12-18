47b173f2a77a0b153778b5d15e8363ee63d2dd9c
Rene Rodriguez <renerod1@hotmail.com>
2024-12-18 01:42:49 -0500

    Completed Advent of Code - 2024 Day 2 Part 2

--- 2024/day02/Java/adventofcode/src/main/java/org/renerod1/adventofcode/day02/App.java
+++ 2024/day02/Java/adventofcode/src/main/java/org/renerod1/adventofcode/day02/App.java
@@ -6,10 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Advent of Code Day 2
@@ -18,13 +16,15 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         part1();
+        part2();
     }
 
     /**
      * Part 1
      */
     public static void part1() {
-        // List<String> list = readFile("2024\\day02\\Java\\adventofcode\\src\\data\\sample_input.txt");
+        // List<String> list =
+        // readFile("2024\\day02\\Java\\adventofcode\\src\\data\\sample_input.txt");
         List<String> list = readFile("2024\\day02\\Java\\adventofcode\\src\\data\\input.txt");
 
         Iterator<String> itr = list.iterator();
@@ -45,7 +45,34 @@ public class App {
         System.out.print(", Lines read: ");
         System.out.println(i.size());
     }
-    
+
+    /**
+     * Part 1
+     */
+    public static void part2() {
+        // List<String> list =
+        // readFile("2024\\day02\\Java\\adventofcode\\src\\data\\sample_input.txt");
+        List<String> list = readFile("2024\\day02\\Java\\adventofcode\\src\\data\\input.txt");
+
+        Iterator<String> itr = list.iterator();
+        Integer safeReports = 0;
+        ArrayList<String> i = new ArrayList<>();
+
+        while (itr.hasNext()) {
+            String line = itr.next();
+            // System.out.println("Line: " + line);
+            i.add(line);
+        }
+
+        safeReports = processReports2(i);
+
+        System.out.print("Part 2: ");
+        System.out.print("Answer: ");
+        System.out.print(safeReports);
+        System.out.print(", Lines read: ");
+        System.out.println(i.size());
+    }
+
     private static Integer processReports(ArrayList<String> i) {
 
         Integer total = 0;
@@ -53,6 +80,18 @@ public class App {
         for (int k = 0; k < i.size(); k++) {
             // System.out.println("i.get(k): " + i.get(k));
             total += checkReport(i.get(k).split(" "));
+        }
+
+        return total;
+    }
+
+    private static Integer processReports2(ArrayList<String> i) {
+
+        Integer total = 0;
+
+        for (int k = 0; k < i.size(); k++) {
+            // System.out.println("\ni.get(k): " + i.get(k));
+            total += checkDampener(i.get(k), -1);
         }
 
         return total;
@@ -89,19 +128,80 @@ public class App {
             }
 
             if (moreThanThree || same || (increasing && decreasing)) {
-
-                // System.out.println("moreThanThree: " + moreThanThree + ", same: " + same + ", increasing: " + increasing
-                //         + ", decreasing: " + decreasing);
+                // System.out.println("moreThanThree: " + moreThanThree + ", same: " + same + ",
+                // increasing: " + increasing
+                // + ", decreasing: " + decreasing);
                 return 0;
             }
         }
 
-        // System.out.println("moreThanThree: " + moreThanThree + ", same: " + same + ", increasing: " + increasing
-        //         + ", decreasing: " + decreasing);
-
+        // System.out.println("moreThanThree: " + moreThanThree + ", same: " + same + ",
+        // increasing: " + increasing
+        // + ", decreasing: " + decreasing);
         return 1;
     }
-    
+
+    private static Integer checkDampener(String og, int position) {
+        int difference = 0;
+        boolean increasing = false;
+        boolean decreasing = false;
+        boolean same = false;
+        boolean moreThanThree = false;
+
+        String i = "";
+        String[] list = og.split(" ");
+
+        if (position != -1) {
+            list[position] = "";
+            i = String.join(" ", list).replaceAll("  ", " ").trim();
+            list = i.split(" ");
+        }
+
+        for (int j = 0; j + 1 < list.length; j++) {
+
+            difference = Integer.parseInt(list[j]) - Integer.parseInt(list[j + 1]);
+            // System.out.println("left: " + list[j] + ", right: " + list[j+1] + ",
+            // difference: " + difference);
+
+            switch (difference) {
+                case -1:
+                case -2:
+                case -3:
+                    increasing = true;
+                    break;
+                case 0:
+                    same = true;
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    decreasing = true;
+                    break;
+                default:
+                    moreThanThree = true;
+                    break;
+            }
+
+            if (moreThanThree || same || (increasing && decreasing)) {
+                // System.out.println("position: " + position + ", list.length: " + list.length
+                // + ", moreThanThree: " + moreThanThree + ", same: " + same + ", increasing: "
+                // + increasing + ", decreasing: " + decreasing);
+
+                if (position >= list.length) {
+                    return 0;
+                }
+
+                position++;
+
+                return checkDampener(og, position);
+            }
+        }
+
+        // System.out.println("moreThanThree: " + moreThanThree + ", same: " + same + ",
+        // increasing: " + increasing + ", decreasing: " + decreasing);
+        return 1;
+    }
+
     private static List<String> readFile(String file) {
         List<String> lines = Collections.emptyList();
         try {
